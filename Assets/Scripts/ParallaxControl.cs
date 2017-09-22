6ef7ed4d4ac92cffda1840d3ad58397aba77cda5
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2017-09-22 15:17:27 -0400

    Updated parallax scrolling

--- Assets/Scripts/ParallaxControl.cs
+++ Assets/Scripts/ParallaxControl.cs
@@ -7,15 +7,23 @@ public class ParallaxControl : MonoBehaviour {
     public float parallaxSpeed = 0.0f;
 
     private Transform mainCamera;
+    private float lastCameraX;
 
 	// Use this for initialization
 	void Start () {
         mainCamera = Camera.main.transform;
+        lastCameraX = mainCamera.position.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         // this.transform.position = mainCamera.transform.position * parallaxSpeed;
-        this.transform.position = new Vector3(mainCamera.position.x, mainCamera.position.y, this.transform.position.z) * parallaxSpeed;
+        // this.transform.position = new Vector3(mainCamera.position.x, mainCamera.position.y, this.transform.position.z) * parallaxSpeed;
+
+        float deltaX = mainCamera.position.x - lastCameraX;
+        transform.position += Vector3.right * (deltaX * parallaxSpeed);
+
+        lastCameraX = mainCamera.position.x;
+        
 	}
 }
