54dfa2f37b31ce0624a2bed93b6f8c7e9b26824a
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2017-10-01 19:29:09 -0400

    Finished Lab 3 and added wk5

--- Assets/Scripts/Wk5/CannonController.cs
+++ Assets/Scripts/Wk5/CannonController.cs
@@ -2,27 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CannonController : MonoBehaviour {
+[System.Serializable]
+public class CannonConfig
+{
+    public float minAngle, maxAngle, rotationSpeed, launchSpeed;
+}
 
-    public float launchSpeed = 1000.0f;
+public class CannonController : MonoBehaviour
+{
+    public CannonConfig cannonConfig;
     public GameObject cannonBall;
     public Transform shotSpawn;
 
     private CameraFollow cameraFollow;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         cameraFollow = Camera.main.GetComponent<CameraFollow>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    }
+
+    // Update is called once per frame
+    void Update()
     {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            // this.transform.Rotate(this.transform.rotation.eulerAngles.x, this.transform.rotation.eulerAngles.y, this.transform.rotation.eulerAngles.z + 0.05f, Space.World);
+            this.transform.Rotate(Vector3.back * cannonConfig.rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Rotate(-Vector3.back * cannonConfig.rotationSpeed * Time.deltaTime);
+        }
+
+
 		if(Input.GetKeyUp(KeyCode.Space) )
         {
             GameObject ball = Instantiate(cannonBall, shotSpawn.position, Quaternion.identity) as GameObject;
             Vector2 forceVec = shotSpawn.transform.position - this.transform.position;
-            forceVec *= launchSpeed;
+            forceVec *= cannonConfig.launchSpeed;
             ball.GetComponent<Rigidbody2D>().AddForce(forceVec);
         }
 	}
---
+++ Assets/Scripts/Wk5/DestryoByTime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestryoByTime : MonoBehaviour {
+
+    public float timeToDestroy = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(this.gameObject, timeToDestroy);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
